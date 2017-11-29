a80ae5ae31a7d2ef662730500d393f296a7915bf
Hassan El Krytly <helkrytly@gmail.com>
2017-11-29 19:26:24 +0000

    addded users controller and sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
   
+  get 'users/new'
+  get 'categories/index'
+
    root 'restaurant_web#home'
    get  '/login',    to: 'restaurant_web#login'
    get  '/contact', to: 'restaurant_web#contact'
-   get  '/menu', to: 'restaurant_web#menu'
-   #get  '/signup',  to: 'users#new'
+   get  '/menu', to: 'catefories#index'
+   get  '/signup',  to: 'users#new'
 
- #resources :food_items
- #resources :categories
- # root 'categories#index'
+ resources :food_items
+ resources :categories
+ root 'categories#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171120030819) do
+ActiveRecord::Schema.define(version: 20171129154659) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 20171120030819) do
     t.string "category_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
