12c9766cd56e9da183e6f2e890276233094da584
Joel Efosa <jefosa@my.ccsu.edu>
2017-11-29 21:13:58 +0000

    Added the sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,24 @@
 Rails.application.routes.draw do
   
+  get 'sessions/new'
+
   get 'users/new'
-  get 'categories/index'
+  
+  
+  #get 'categories/index'
 
    root 'restaurant_web#home'
    get  '/login',    to: 'restaurant_web#login'
    get  '/contact', to: 'restaurant_web#contact'
    get  '/menu', to: 'catefories#index'
    get  '/signup',  to: 'users#new'
+   get  '/users', to: 'index#users_path'
+   get  '/users/:id(.:format)', to: 'show#users_path(users_path)'
+   post '/users(.:format)', to: 'users#create'
+   post 'signup', to: 'users#create'
+   resources :users
 
  resources :food_items
- resources :categories
- root 'categories#index'
+ #resources :categories
+ #root 'categories#index'
 end
