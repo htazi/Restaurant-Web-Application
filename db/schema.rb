93aa1db752c42269a29034a5230ff5487e70abe5
Hassan El Krytly <helkrytly@gmail.com>
2017-12-01 03:54:06 +0000

    added Contact page

--- db/schema.rb
+++ db/schema.rb
@@ -33,7 +33,6 @@ ActiveRecord::Schema.define(version: 20171129154659) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
-    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
