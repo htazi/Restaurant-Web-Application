398b6b25745e74bfd8056252194aec0d06739ed3
William Bellenger <bellenger@my.ccsu.edu>
2017-12-06 06:30:36 +0000

    Fixed select_Category link
    The link to select_category from category_list now properly sends over the @category

--- app/controllers/category_list_controller.rb
+++ app/controllers/category_list_controller.rb
@@ -4,6 +4,10 @@ class CategoryListController < ApplicationController
     @category = Category.find(params[:id])
   end
   
+  def create_order
+    @category = Category.find(params[:id])
+  end
+  
   def select_category
     @categories = Category.all
   end
